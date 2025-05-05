faa7594120ba2e9056f5e5a8be53253d4ab2f765
kathleens_13 <kathleen.a.schenck@gmail.com>
2025-05-05 12:34:56 -0400

    made player shape

--- main.py
+++ main.py
@@ -3,23 +3,30 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+# initiate different parts that will be needed
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+
+# main game loop
     while True:
         #check for quit events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
         screen.fill("black")
+        player.draw(screen)
+        dt = game_clock.tick(60) / 1000
         
         pygame.display.flip()
 
